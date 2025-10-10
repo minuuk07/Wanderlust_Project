b0bdd5daff29d31404a34a11639cddb60c789562
MRINMOY GHOSH <m74777abc@gmail.com>
2025-10-10 18:25:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 if(process.env.NODE_ENV!="production"){
 require('dotenv').config();
 }
-const cloudinary = require('cloudinary').v2;
-const multer = require('multer');
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
 console.log(process.env.SECRET);
 const express=require("express");
 const app=express();
