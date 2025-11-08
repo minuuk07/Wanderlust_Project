4b5d2368a5f713da345a69230e8325238d651ca1
MRINMOY GHOSH <m74777abc@gmail.com>
2025-11-08 18:45:39 +0530

    Update index.js

--- router/index.js
+++ router/index.js
@@ -1,16 +1,9 @@
 // routes/index.js
 const express = require("express");
 const router = express.Router();
-const wrapAsync=require("../utils/wrapAsync.js");
-
-const listing=require("../models/listing.js");
 const homeController = require("../controller/homeController.js");
 
 router.get("/", homeController.home);
 router.get("/filter/:category", homeController.filterByCategory);
 
 module.exports = router;
-
-
-
-
