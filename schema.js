cd8a9147bc9e9b356b34947a372fa3ea93828c24
MRINMOY GHOSH <m74777abc@gmail.com>
2025-11-08 16:44:08 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -19,6 +19,7 @@ module.exports.listingSchema = Joi.object({
     title: Joi.string().required(),
     description: Joi.string().required(),
     location: Joi.string().required(),
+    category: Joi.string().required(),
     country: Joi.string().required(),
     price: Joi.number().min(0).required(),
     image: Joi.object({
