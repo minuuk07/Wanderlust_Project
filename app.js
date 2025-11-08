7f2c02716e405557c8218d515ac1cce4e2d20e17
MRINMOY GHOSH <m74777abc@gmail.com>
2025-11-08 23:13:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(method("_method"));
 app.engine('ejs', ejsmate);
 app.use(express.static(path.join(__dirname,"public")));
-
+app.use(express.json());
  
 const store=MongoStore.create({
   // change here full change
