8ff4942e284784eb7c8ee0eb77158356c24a7435
MRINMOY GHOSH <m74777abc@gmail.com>
2026-02-27 14:27:47 +0530

    Update meddlewear.js

--- meddlewear.js
+++ meddlewear.js
@@ -27,7 +27,7 @@ module.exports.isOwner = async (req, res, next) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
 
-  if (!listing.owner._id.equals(res.locals.currUSer._id)) {
+  if (!listing.owner._id.equals(res.locals.currUser._id)) {
     req.flash("error", "You don't have permission ");
     return res.redirect(`/listings/${id}`);
   }
