1781a6754deddda313daf005187629b05f20d937
MRINMOY GHOSH <m74777abc@gmail.com>
2026-02-27 11:29:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({extended:true}));
+app.set("trust proxy", 1);
 app.use(method("_method"));
 app.engine('ejs', ejsmate);
 app.use(express.static(path.join(__dirname,"public")));
