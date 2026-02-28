089f5d1abbbdee9c33ee9556e9d32d69772ff21e
MRINMOY GHOSH <m74777abc@gmail.com>
2026-03-01 03:10:34 +0530

    Add files via upload

---
+++ models/message.js
@@ -0,0 +1,30 @@
+// models/message.js
+
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    message: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("Message", messageSchema);
