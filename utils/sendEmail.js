5be50c8dd0e0ac605888ea091e32c32fcdbe05ca
MRINMOY GHOSH <m74777abc@gmail.com>
2026-03-01 22:29:06 +0530

    Add files via upload

---
+++ utils/sendEmail.js
@@ -0,0 +1,24 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (to, otp) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "m74777abcd@gmail.com",
+      pass: "gwgniornhkfzimmd"
+    }
+  });
+
+  await transporter.sendMail({
+    from: `Wanderlust <${"m74777abcd@gmail.com"}>`,
+    to,
+    subject: "Reset Password OTP",
+    html: `
+      <h2>Password Reset OTP</h2>
+      <h1>${otp}</h1>
+      <p>This OTP is valid for 5 minutes.</p>
+    `
+  });
+};
+
+module.exports = sendEmail;
