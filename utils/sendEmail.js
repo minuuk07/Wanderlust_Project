387255266e6295a1cc0f65b0c77aca050e49d05c
MRINMOY GHOSH <m74777abc@gmail.com>
2026-03-01 23:14:11 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,24 +2,25 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, otp) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
-      user: "m74777abc@gmail.com",
-      pass: "hkvuibvbgtfmsedu"
-    }
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
   });
 
   await transporter.sendMail({
-    from: `Wanderlust <${"m74777abc@gmail.com"}>`,
+    from: `Wanderlust <${process.env.EMAIL_USER}>`,
     to,
     subject: "Reset Password OTP",
     html: `
       <h2>Password Reset OTP</h2>
       <h1>${otp}</h1>
       <p>This OTP is valid for 5 minutes.</p>
-    `
+    `,
   });
 };
-
 
 module.exports = sendEmail;
